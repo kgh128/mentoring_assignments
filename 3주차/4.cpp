171f87f765a6b896f387fcbc98633e416f06a43f
HayoungKim20 <hayoung@DESKTOP-U43EIMM.localdomain>
2021-02-18 21:04:15 +0900

    Implement Calculator to 4.cpp

--- 3주차/4.cpp
+++ 3주차/4.cpp
@@ -18,31 +18,73 @@ class Calculator {
 };
 
 void Calculator:: Init() {
-    
+	numOfAdd = 0;
+	numOfMin = 0;
+	numOfMul = 0;
+	numOfDiv = 0;	
 }
 
-double Calculator::Add(double num1, double num2) {
-    
+double Calculator::Add(double num1, double num2) { 
+	numOfAdd++;
+	return (num1 + num2);
 }
 
 double Calculator::Min(double num1, double num2) {
-    
+    numOfMin++;
+	return (num1 - num2);
 }
 
 double Calculator::Mul(double num1, double num2) {
-    
+    numOfMul++;
+	return (num1 * num2);
 }
 
 double Calculator::Div(double num1, double num2) {
-   
+	numOfDiv++;
+	return (num1 / num2);
 }
 
 void Calculator::ShowOpCount() {
-    
+	cout << '\n';
+    cout << "덧셈: " << numOfAdd;
+	cout << " 뺄셈: " << numOfMin;
+	cout << " 곱셈: " << numOfMul;
+	cout << " 나눗셈: " << numOfDiv;
+	cout << '\n';
 }
 
 int main(void){
     Calculator cal;
+	cal.Init();
+	double num1 = 0, num2 = 0;
+	char op = 0, quit = 'n';
     
-    //  출력 결과와 같은 값이 나오도록 연산 진행
+	while ( quit != 'y' ) {
+		cin >> num1;
+		cin >> op;
+		cin >> num2;
+
+		switch(op) {
+			case '+' : 
+				cout << " = " << cal.Add(num1, num2) << '\n';
+				break;
+			case '-' :
+				cout << " = " << cal.Min(num1, num2) << '\n';
+				break;
+			case '*' :
+				cout << " = " << cal.Mul(num1, num2) << '\n';
+				break;
+			case '/' :
+				if (num2 == 0) {
+					cal.Div(num1, num2);
+					cout << " = " << "Error" << '\n';
+				}
+				else
+					cout << " = " << cal.Div(num1, num2) << '\n';
+		}
+
+		cout << "                종료? (y / n) : ";
+		cin >> quit;
+	}
+	cal.ShowOpCount();
 }

455df092fbeedb5a6e1b606e59b246db0ff8570e
HayoungKim20 <hayoung@DESKTOP-U43EIMM.localdomain>
2021-02-23 22:06:47 +0900

    Fix 3.c

--- 4주차_실습/3.c
+++ 4주차_실습/3.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(void) {
-	printf("\"\/*\\은 개행문자 입니다.*\/\"\n");
+	printf("\"/*\\은 개행문자 입니다.*/\"\n");
 	printf("\\\\\'%%d\'는 10진수 정수를 출력하기 위한 변환명세 입니다.\\\\\n");
    return 0;
 }   
